3cd476eb08dd59718c53c3b4ea2a6c7d9b38d7fb
Ayush Kumar <ayushdotdev@gmail.com>
2023-02-07 12:46:17 +0530

    commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,12 @@
 import Head from 'next/head'
 import '../styles/globals.css'
 import type { AppProps } from "next/app"
-
+import { DefaultSeo } from "next-seo"
+import { seo } from '@/lib/seo'
 export default function App({ Component, pageProps }:AppProps) {
   return (
     <>
-    <Head>
-      <title>
-        Ayush Kumar
-      </title>
-    </Head>
+    <DefaultSeo {...seo}/>
     <Component {...pageProps} />
     </>
   )
